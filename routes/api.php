752a934c422437ecd76d2da3d25902e38530a496
RidhoPajri <ridhofajriawan16@gmail.com>
2024-07-25 17:10:27 +0700

    wk

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-
-
 Route::middleware(['auth:sanctum'])->group(function(){
     Route::post('logout',[AuthController::class,'logout']);
     // route / endpoint kategori
